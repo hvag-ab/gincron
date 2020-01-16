ad25580f187cc4753d38bc6278c67aa061df53fd
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2020-01-16 12:13:12 +0800

    添加UpdateStatus方法

--- models/task.go
+++ models/task.go
@@ -46,6 +46,26 @@ func (t *Task) Update() bool {
 
 }
 
+func (t *Task) UpdateStatus() bool {
+	/*
+	这里需要注意，Update会自动从user结构体中提取非0和非nil得值作为需要更新的内容，因此，如果需要更新一个值为0，则此种方法将无法实现，因此有两种选择：
+1.通过添加Cols函数指定需要更新结构体中的哪些值，未指定的将不更新，指定了的即使为0也会更新。
+affected, err := engine.Id(id).Cols("age").Update(&user)
+2.通过传入map[string]interface{}来进行更新，但这时需要额外指定更新到哪个表，因为通过map是无法自动检测更新哪个表的。
+affected, err := engine.Table(new(User)).Id(id).Update(map[string]interface{}{"age":0})
+	*/
+	affected, err := db.DB.Table("task").Where("task.id=?",t.Id).Update(map[string]interface{}{"status":t.Status})
+	if err != nil {
+		return false
+	}
+	if affected == 0 {
+		return false
+	}else{
+		return true
+	}
+
+}
+
 func TaskAdd(task *Task) (int64, bool) {
 	if task.TaskName == "" {
 		return 0, false
