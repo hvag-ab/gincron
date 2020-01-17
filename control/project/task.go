05d336632353633747b042c871d81fccbd65ab99
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2020-01-17 14:28:35 +0800

    Update task.go

--- control/project/task.go
+++ control/project/task.go
@@ -370,7 +370,7 @@ func Logs(c *gin.Context) {
 	list := make([]map[string]interface{}, len(result))
 	for k, v := range result {
 		row := make(map[string]interface{})
-		row["id"] = v.TaskId
+		row["task_id"] = v.TaskId
 		row["start_time"] = time.Unix(v.CreateTime, 0).Format("2006-01-02 15:04:05")
 		row["process_time"] = float64(v.ProcessTime) / 1000
 		row["ouput_size"] = v.Output
